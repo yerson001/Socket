8c51f0422e22859097a969ca9024f4b222eaec5c
Yerson sanchez <37232721+yerson001@users.noreply.github.com>
2021-03-28 15:24:45 -0500

    Add files via upload

--- client.cpp
+++ client.cpp
@@ -1,57 +1,60 @@
  /* Client code in C */
  
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+  #include <sys/types.h>
+  #include <sys/socket.h>
+  #include <netinet/in.h>
+  #include <arpa/inet.h>
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <string.h>
+  #include <unistd.h>
  
-int main(void)
-{
-  struct sockaddr_in stSockAddr;
-  int Res;
-  int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-  int n;
- 
-  if (-1 == SocketFD)
+  int main(void)
   {
-    perror("cannot create socket");
-    exit(EXIT_FAILURE);
-  }
+    struct sockaddr_in stSockAddr;
+    int Res;
+    int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int n;
  
-  memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+    if (-1 == SocketFD)
+    {
+      perror("cannot create socket");
+      exit(EXIT_FAILURE);
+    }
  
-  stSockAddr.sin_family = AF_INET;
-  stSockAddr.sin_port = htons(45000);
-  Res = inet_pton(AF_INET, "127.0.0.1", &stSockAddr.sin_addr);
+    //memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
  
-  if (0 > Res)
-  {
-    perror("error: first parameter is not a valid address family");
+    stSockAddr.sin_family = AF_INET;
+    stSockAddr.sin_port = htons(45000);
+    Res = inet_pton(AF_INET, "127.0.0.1", &stSockAddr.sin_addr);
+ 
+    if (0 > Res)
+    {
+      perror("error: first parameter is not a valid address family");
+      close(SocketFD);
+      exit(EXIT_FAILURE);
+    }
+    else if (0 == Res)
+    {
+      perror("char string (second parameter does not contain valid ipaddress");
+      close(SocketFD);
+      exit(EXIT_FAILURE);
+    }
+ 
+    if (-1 == connect(SocketFD, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
+    {
+      perror("connect failed");
+      close(SocketFD);
+      exit(EXIT_FAILURE);
+    }
+    n = write(SocketFD,"Hi, this is yrsn.",18);
+    /* perform read write operations ... */
+     char buffer[256];
+     n = read(SocketFD,buffer,100);
+     printf("%s\n",buffer);
+
+    shutdown(SocketFD, SHUT_RDWR);
+ 
     close(SocketFD);
-    exit(EXIT_FAILURE);
+    return 0;
   }
-  else if (0 == Res)
-  {
-    perror("char string (second parameter does not contain valid ipaddress");
-    close(SocketFD);
-    exit(EXIT_FAILURE);
-  }
- 
-  if (-1 == connect(SocketFD, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
-  {
-    perror("connect failed");
-    close(SocketFD);
-    exit(EXIT_FAILURE);
-  }
-  n = write(SocketFD,"Hola como estas",18);//SOMAXCONN
-  /* perform read write operations ... */
- 
-  shutdown(SocketFD, SHUT_RDWR);
- 
-  close(SocketFD);
-  return 0;
-}
--- server.cpp
+++ server.cpp
@@ -1,74 +1,73 @@
   /* Server code in C */
  
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <iostream>
-
-int main(void)
-{
-  struct sockaddr_in stSockAddr;
-  int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-  char buffer[256];
-  int n;
+  #include <sys/types.h>
+  #include <sys/socket.h>
+  #include <netinet/in.h>
+  #include <arpa/inet.h>
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <string.h>
+  #include <unistd.h>
  
-  if(-1 == SocketFD)
+  int main(void)
   {
-    std::cout<<"can not create socket"<<std::endl;
-    exit(EXIT_FAILURE);
-  }
+    struct sockaddr_in stSockAddr;
+    int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    char buffer[256];
+    int n;
  
-  memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
- 
-  stSockAddr.sin_family = AF_INET;
-  stSockAddr.sin_port = htons(45000);
-  stSockAddr.sin_addr.s_addr = INADDR_ANY;
- 
-  if(-1 == bind(SocketFD,(const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
-  {
-    std::cout<<"error bind failed"<<std::endl;
-    close(SocketFD);
-    exit(EXIT_FAILURE);
-  }
- 
-  if(-1 == listen(SocketFD, 10))
-  {
-    std::cout<<"error listen failed"<<std::endl;
-    close(SocketFD);
-    exit(EXIT_FAILURE);
-  }
- 
-  for(;;)
-  {
-    int ConnectFD = accept(SocketFD, NULL, NULL);
- 
-    if(0 > ConnectFD)
+    if(-1 == SocketFD)
     {
-      std::cout<<"error accept failed"<<std::endl;
+      perror("can not create socket");
+      exit(EXIT_FAILURE);
+    }
+ 
+    memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+ 
+    stSockAddr.sin_family = AF_INET;
+    stSockAddr.sin_port = htons(45000);
+    stSockAddr.sin_addr.s_addr = INADDR_ANY;
+ 
+    if(-1 == bind(SocketFD,(const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
+    {
+      perror("error bind failed");
       close(SocketFD);
       exit(EXIT_FAILURE);
     }
  
+    if(-1 == listen(SocketFD, 10))
+    {
+      perror("error listen failed");
+      close(SocketFD);
+      exit(EXIT_FAILURE);
+    }
  
-    bzero(buffer,256);
-    n = read(ConnectFD,buffer,255);
-    if (n < 0) std::cout<<"ERROR reading from socket"<<std::endl;
-    printf("El mensaje: [%s]\n",buffer);
-    n = write(ConnectFD,"I got your message",18);
-    if (n < 0) std::cout<<"ERROR writing to socket"<<std::endl;
+    for(;;)
+    {
+      int ConnectFD = accept(SocketFD, NULL, NULL);
+ /*
+      if(0 > ConnectFD)
+      {
+        perror("error accept failed");
+        close(SocketFD);
+        exit(EXIT_FAILURE);
+      }
+ */
  
-    /* perform read write operations ... */
+     bzero(buffer,256);
+     n = read(ConnectFD,buffer,255);
+     if (n < 0) perror("ERROR reading from socket");
+     printf("Here is the message: [%s]\n",buffer);
+     n = write(ConnectFD,"I got your message",18);
+     if (n < 0) perror("ERROR writing to socket");
  
-    shutdown(ConnectFD, SHUT_RDWR);
+     /* perform read write operations ... */
  
-    close(ConnectFD);
+      shutdown(ConnectFD, SHUT_RDWR);
+ 
+      close(ConnectFD);
+    }
+ 
+    close(SocketFD);
+    return 0;
   }
- 
-  close(SocketFD);
-  return 0;
-}
