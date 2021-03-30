d608f5039d6eb0a3a5957892d18e045bc907254d
yhon.sanchez@ucsp.edu.pe <yerson001>
2021-03-29 23:53:00 -0500

    chat2.0

--- chat/p1.cpp
+++ chat/p1.cpp
@@ -1,60 +1,59 @@
-  #include <iostream>
-  #include <sys/types.h>
-  #include <sys/socket.h>
-  #include <netinet/in.h>
-  #include <arpa/inet.h>
-  #include <stdio.h>
-  #include <stdlib.h>
-  #include <string.h>
-  #include <unistd.h>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-  using namespace std;
+using namespace std;
 
 
-  int main(void)
+int main(void)
+{
+  struct sockaddr_in stSockAddr;
+  int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  char buffer[256];
+  int n;
+  string txtserv;
+    
+  if(-1 == SocketFD)
   {
-    struct sockaddr_in stSockAddr;
-    int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    char buffer[256];
-    int n;
-    string txtserv;
+    perror("can not create socket");
+    exit(EXIT_FAILURE);
+  }
+
+  memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+
+  stSockAddr.sin_family = AF_INET;
+  stSockAddr.sin_port = htons(45000);
+  stSockAddr.sin_addr.s_addr = INADDR_ANY;
+  //para cerrar x comsola el puerto 8083  
+  /*
+    ps aux | grep 8083
+    */
     
-    if(-1 == SocketFD)
-    {
-      perror("can not create socket");
-      exit(EXIT_FAILURE);
-    }
+  //debemos linkear(bind) el stSockAddr con el socket
+  std::cout<<"-------Servidor-------"<<std::endl;      
+  if(-1 == bind(SocketFD,(const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
+  {
+    perror("error bind failed");
+    close(SocketFD);
+    exit(EXIT_FAILURE);
+  }
 
-    memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
+  if(-1 == listen(SocketFD, 10))
+  {
+    perror("error listen failed");
+    close(SocketFD);
+    exit(EXIT_FAILURE);
+  }
 
-    stSockAddr.sin_family = AF_INET;
-    stSockAddr.sin_port = htons(45000);
-    stSockAddr.sin_addr.s_addr = INADDR_ANY;
-    //para cerrar x comsola el puerto 8083  
-    /*
-     ps aux | grep 8083
-     */
-    
-    //debemos linkear(bind) el stSockAddr con el socket
-    print("-------Servidor-------")      
-    if(-1 == bind(SocketFD,(const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
-    {
-      perror("error bind failed");
-      close(SocketFD);
-      exit(EXIT_FAILURE);
-    }
-
-    if(-1 == listen(SocketFD, 10))
-    {
-      perror("error listen failed");
-      close(SocketFD);
-      exit(EXIT_FAILURE);
-    }
-
-    for(;;)
-    {
-        
-      int ClientFD = accept(SocketFD, NULL, NULL);
+  for(;;)
+  {
+    int ClientFD = accept(SocketFD, NULL, NULL);
     do{
       if(0 > ClientFD )
       {
@@ -64,27 +63,27 @@
       }
 
     
-     bzero(buffer,256);   ///clean up the  buffer
-     n = read(ClientFD ,buffer,255);
+    bzero(buffer,256);   ///clean up the  buffer
+    n = read(ClientFD ,buffer,255);
 
      /**
        n = 0 // no hay nada por recibir
      */
     
-     if (n < 0) perror("ERROR reading from socket");
-     printf("CLIENT: [%s]\n",buffer);
-     cout<<"SERVER: ";
-     cin >>txtserv;
-     n = write(ClientFD,txtserv.c_str(), txtserv.length() );//"I got your message",18);
-     if (n < 0) perror("ERROR writing to socket");
-     }while(strncmp(buffer,"BYE",3)!=0);
+    if (n < 0) perror("ERROR reading from socket");
+    printf("CLIENT: [%s]\n",buffer);
+    cout<<"SERVER: ";
+    cin >>txtserv;
+    n = write(ClientFD,txtserv.c_str(), txtserv.length() );//"I got your message",18);
+    if (n < 0) perror("ERROR writing to socket");
+    }while(strncmp(buffer,"BYE",3)!=0);
      /* perform read write operations ... */
 
-      shutdown(ClientFD , SHUT_RDWR);
+    shutdown(ClientFD , SHUT_RDWR);
 
-      close(ClientFD);
-    }
-
-    close(SocketFD);
-    return 0;
+    close(ClientFD);
   }
+
+  close(SocketFD);
+  return 0;
+}
--- chat/p2.cpp
+++ chat/p2.cpp
@@ -1,63 +1,64 @@
-  #include <sys/types.h>
-  #include <iostream>
-  #include <sys/socket.h>
-  #include <netinet/in.h>
-  #include <arpa/inet.h>
-  #include <stdio.h>
-  #include <stdlib.h>
-  #include <string.h>
-  #include <unistd.h>
+// client  
+#include <sys/types.h>
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
   
   
-  using namespace std;
+using namespace std;
 
-  int main(void)
+int main(void)
+{
+  struct sockaddr_in stSockAddr;  //define caracteristicas de comunicacion
+  int Res;
+  int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP); //fd file descriptor, pf inet socket para internet
+  int n;
+  char buffer[1000];
+
+  if (-1 == SocketFD)
   {
-    struct sockaddr_in stSockAddr;  //define caracteristicas de comunicacion
-    int Res;
-    int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP); //fd file descriptor, pf inet socket para internet
-    int n;
-    char buffer[1000];
-
-    if (-1 == SocketFD)
-    {
-      perror("cannot create socket");
-      exit(EXIT_FAILURE);
-    }
-    string txt;
+    perror("cannot create socket");
+    exit(EXIT_FAILURE);
+  }
+  string txt;
     
     
     
     //memset(&stSockAddr, 0, sizeof(struct sockaddr_in));  //limpia stSockAddr
 
-    stSockAddr.sin_family = AF_INET;
-    stSockAddr.sin_port = htons(45000); //45001
-    Res = inet_pton(AF_INET, "127.0.0.1", &stSockAddr.sin_addr);  //identifica el puerto del servidor
+  stSockAddr.sin_family = AF_INET;
+  stSockAddr.sin_port = htons(45000); //45001
+  Res = inet_pton(AF_INET, "127.0.0.1", &stSockAddr.sin_addr);  //identifica el puerto del servidor
 
     //validdation of assining
-    if (0 > Res)
-    {
-      perror("error: first parameter is not a valid address family");
-      close(SocketFD);
-      exit(EXIT_FAILURE);
-    }
-    else if (0 == Res)
-    {
-      perror("char string (second parameter does not contain valid ipaddress");
-      close(SocketFD);
-      exit(EXIT_FAILURE);
-    }
+  if (0 > Res)
+  {
+    perror("error: first parameter is not a valid address family");
+    close(SocketFD);
+    exit(EXIT_FAILURE);
+  }
+  else if (0 == Res)
+  {
+    perror("char string (second parameter does not contain valid ipaddress");
+    close(SocketFD);
+    exit(EXIT_FAILURE);
+  }
     //end of validation of assining
 
-    if (-1 == connect(SocketFD, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
-    {
-      perror("connect failed");
-      close(SocketFD);
-      exit(EXIT_FAILURE);
-    }
-    do{
+  if (-1 == connect(SocketFD, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
+  {
+    perror("connect failed");
+    close(SocketFD);
+    exit(EXIT_FAILURE);
+  }
+  do{
     cout<<"CLIENT: ";
-	cin >>txt;
+    cin >>txt;
      n = write(SocketFD, txt.c_str(),txt.length());//"ximena",6);
     /* perform read write operations ... */
 
@@ -69,8 +70,8 @@
      
     }while (strncmp(buffer,"BYE",3) !=0);
 
-    shutdown(SocketFD, SHUT_RDWR);
+  shutdown(SocketFD, SHUT_RDWR);
 
-    close(SocketFD);
-    return 0;
-  }
+  close(SocketFD);
+  return 0;
+}
